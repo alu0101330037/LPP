b3bfca6c0d7aab8303ccfd285b049c0ba93d0c9e
Jorge Ruiz Padilla <alu0101330037@ull.edu.es>
2022-06-13 18:07:24 +0100

    Probado el método de multiplicar por un escalar y falló

--- Practica_6/spec/Matrix_spec.rb
+++ Practica_6/spec/Matrix_spec.rb
@@ -13,5 +13,8 @@ describe Matrix do
     expect(Matrix.new(2,3,[1,2,3,4,5,6]).suma([1,2,3,4,5,6])).to eq([2,4,6,8,10,12]) #matcher
   end
 
-  
+  it "Se multiplica una matriz por un escalar y se muestra por pantalla" do
+    # Define la expectativa
+    expect(Matrix.new(2, 3, [1,1,1,1,1,1]).prod_esc(3)).to eq([3,3,3,3,3,3]) #matcher
+  end  
 end
