a649cc671d6fd36d018ea115855159a2f7f19553
Jorge Ruiz Padilla <alu0101330037@ull.edu.es>
2022-06-13 17:52:46 +0100

    Probado el método suma y falló

--- Practica_6/spec/Matrix_spec.rb
+++ Practica_6/spec/Matrix_spec.rb
@@ -8,5 +8,8 @@ describe Matrix do
     # Define la expectativa
     expect(Matrix.new(2,3,[1,2,3,4,5,6]).to_s).to eq([1, 2, 3, 4, 5, 6]) #matcher
   end
-
+  it "Se suman dos matrices y se muestra por pantalla" do
+    # Define la expectativa
+    expect(Matrix.new(2,3,[1,2,3,4,5,6]).suma([1,2,3,4,5,6]).to_s).to eq([2,4,6,8,10,12]) #matcher
+  end
 end
