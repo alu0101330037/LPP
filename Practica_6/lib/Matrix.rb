e10d83a5d44139fbfc44ea3226d33f429ac17596
Jorge Ruiz Padilla <alu0101330037@ull.edu.es>
2022-06-13 18:18:55 +0100

    Creado el método de trasponer una matriz y funcionó

--- Practica_6/lib/Matrix.rb
+++ Practica_6/lib/Matrix.rb
@@ -24,4 +24,23 @@ class Matrix
         end
         to_s
     end
+
+    def transp
+        i = 0
+        j = 0
+        matrx_aux=Array.new(@x*@y)
+        while (i < @x) do
+          while (j < @y) do
+             matrx_aux[j*@x + i] = @z[i*@y + j]
+             j+=1
+          end
+          j=0
+          i+=1
+        end
+        aux = @x
+        @x = @y
+        @y = aux
+        @z = matrx_aux
+        to_s
+    end
 end
